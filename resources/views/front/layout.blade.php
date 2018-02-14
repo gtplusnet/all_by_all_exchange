1e91d05694b99d9b134f592999080da764e211f9
Rommel Cuneta <cunetarommel0620@gmail.com>
2018-02-14 17:02:22 +0800

    mel

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -4,8 +4,8 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" href="/assets/img/allbyall-ico.png"" type="image/png"/>
-	<title>Allbyall</title>
+	<link rel="icon" href="/assets/img/allbyall-ico.png" type="image/png"/>
+	<title>Allbyall Exchange</title>
 	
 	<!-- Google Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700" rel="stylesheet">
