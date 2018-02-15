8aa3ad6c040ab5a025116754be420c1e90fa0a2d
Rommel Cuneta <cunetarommel0620@gmail.com>
2018-02-15 13:53:15 +0800

    fix process layout

--- resources/views/member/cash_in.blade.php
+++ resources/views/member/cash_in.blade.php
@@ -9,17 +9,22 @@
 		<div class="content-wrapper">
 			<div class="container">
 				<div class="holder">
-					<div class="head-text row no-gutters clearfix">
+					{{-- <div class="head-text row no-gutters clearfix">
 						<div class="col-md-4 col-sm-4 col-xs-4"><div class="text-container t-yellow">Method</div></div>
 						<div class="col-md-4 col-sm-4 col-xs-4"><div class="text-container t-center">Amount</div></div>
 						<div class="col-md-4 col-sm-4 col-xs-4"><div class="text-container t-right">Payment</div></div>
+					</div> --}}
+					<div class="head-text d-flex justify-content-between m-auto">
+						<div><div class="text-container t-yellow">Method</div></div>
+						<div><div class="text-container">Amount</div></div>
+						<div><div class="text-container">Payment</div></div>
 					</div>
 					<div class="head-text-img t-center"><img src="/assets/img/c-step1.png"></div>
-					<div class="head-text-2 row no-gutters clearfix pt-35">
+					<div class="head-text-2 row no-gutters clearfix mt-4">
 						<div class="col-md-3 col-sm-3 col-xs-3"><div class="head-text-img2"><img src="/assets/img/cashin-vector.png"></div></div>
 						<div class="col-md-9 col-sm-9 col-xs-9"><div class="text-container">How would you like to Cash-In?</div></div>
 					</div>
-					<div class="cash-option row clearfix">
+					<div class="cash-option mt-3 row clearfix">
 						<div class="col-md-6 col-sm-6 col-xs-6">
 							<div class="per-option t-center">
 								<div class="icon-container"><img src="/assets/img/bank-depo.png"></div>
@@ -35,7 +40,7 @@
 							</div>
 						</div>
 					</div>
-					<p class="p-sm t-center pt-25">
+					<p class="p-sm t-center mt-5">
 						Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
 					</p>
 				</div>
--- resources/views/member/cash_out.blade.php
+++ resources/views/member/cash_out.blade.php
@@ -9,18 +9,24 @@
 		<div class="content-wrapper">
 			<div class="container">
 				<div class="holder">
-					<div class="head-text row no-gutters clearfix">
+					{{-- <div class="head-text row no-gutters clearfix">
 						<div class="col-md-3 col-sm-3 col-xs-3"><div class="text-container t-center t-yellow">Method</div></div>
 						<div class="col-md-3 col-sm-3 col-xs-3"><div class="text-container t-center">Amount</div></div>
 						<div class="col-md-3 col-sm-3 col-xs-3"><div class="text-container t-center">Recipient</div></div>
 						<div class="col-md-3 col-sm-3 col-xs-3"><div class="text-container t-center">Payment</div></div>
+					</div> --}}
+					<div class="head-text d-flex justify-content-between m-auto">
+						<div><div class="text-container t-yellow">Method</div></div>
+						<div><div class="text-container">Amount</div></div>
+						<div><div class="text-container">Recipient</div></div>
+						<div><div class="text-container">Payment</div></div>
 					</div>
 					<div class="head-text-img t-center"><img src="/assets/img/c2-step1.png"></div>
-					<div class="head-text-2 row no-gutters clearfix pt-35">
+					<div class="head-text-2 row no-gutters clearfix mt-4">
 						<div class="col-md-3 col-sm-3 col-xs-3"><div class="head-text-img2"><img src="/assets/img/cashout-vector.png"></div></div>
 						<div class="col-md-9 col-sm-9 col-xs-9"><div class="text-container">How should we send the money?</div></div>
 					</div>
-					<div class="cash-option row clearfix pt-20">
+					<div class="cash-option row clearfix mt-5">
 						<div class="col-md-4 col-sm-4 col-xs-4">
 							<div class="per-option t-center match-height">
 								<div class="icon-container"><img src="/assets/img/bank-depo.png"></div>
@@ -43,7 +49,7 @@
 							</div>
 						</div>
 					</div>
-					<p class="p-sm t-center pt-25">
+					<p class="p-sm t-center mt-5">
 						Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
 					</p>
 				</div>
