92d496e93b2b4c1847efc102ef3c61537c8d722d
Rommel Cuneta <cunetarommel0620@gmail.com>
2018-02-15 12:09:06 +0800

    sample

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -13,6 +13,7 @@
 					<div class="button-container">
 						<button onclick="location.href='/member/login'" class="btn btn-login" type="submit">LOGIN</button>
 					</div>
+					<div class="sample"></div>
 					<div class="button-container">
 						<button onclick="location.href='/member/register'" class="btn btn-create-account" type="submit">CREATE AN ACCOUNT</button>
 					</div>
